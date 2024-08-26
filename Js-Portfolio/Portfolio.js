f94d4c13b3e6781004dfc0f60067c67da8f7e59f
UtitBest <utitbesta@gmail.com>
2024-08-26 10:36:08 +0100

    second commit

---
+++ Js-Portfolio/Portfolio.js
@@ -0,0 +1,133 @@
+if(navigator.onLine == true){
+    ScrollReveal({
+        distance:'80px',
+        duration:2000,
+        delay:200
+    });
+    ScrollReveal().reveal('.Imageise', {origin:'right'});
+    ScrollReveal().reveal('.murerr', {origin:'left'});
+    ScrollReveal().reveal('.thesmae', {origin:'bottom'});    
+    ScrollReveal().reveal('.rada1', {origin:'left'});
+    ScrollReveal().reveal('.rada2', {origin:'right'});
+    ScrollReveal().reveal('.to_me3', {origin:'top'});
+    ScrollReveal().reveal('.to_me4', {origin:'bottom'});
+    ScrollReveal().reveal('.jejeje', {origin:'center'});
+    ScrollReveal().reveal('.me_love', {origin:'bottom'}); 
+    ScrollReveal().reveal('.tapines', {origin:'bottom'});
+    ScrollReveal().reveal('.Forform', {origin:'right'});
+    ScrollReveal().reveal('.contactwrap', {origin:'left'});
+}
+
+let rtnTxr = document.querySelector('.ueryyryry');
+let txtarry = ["I\'m a Web Developer An Graphic Designer", 'Am a full stack developer', 'I code with, HTML, CSS, BOOTSTRAP, JAVASCRIPT, JQUERY, PHP AND MYSQL'];
+let starIndex = 0;
+let writeArray = 0;
+
+setInterval(function(){
+    let txtInside = rtnTxr.innerHTML;
+    if(starIndex >= txtarry[writeArray].length && rtnTxr.getAttribute('typing') == 'true'){
+        rtnTxr.innerHTML = txtInside.toString().slice(0, -1);
+        let textInsideLLenght = txtInside.length;
+
+        if(textInsideLLenght <= 0){
+            writeArray++;
+            starIndex = 0;
+            rtnTxr.setAttribute('typing', false)
+        }
+    }
+    if(writeArray >= txtarry.length){
+        writeArray = 0; 
+    }
+    rtnTxr.innerHTML += txtarry[writeArray].charAt(starIndex);
+    rtnTxr.setAttribute('typing', true)
+    starIndex++;    
+}, 200);
+
+
+
+window.addEventListener('scroll', function(){
+    var scrolling = document.querySelectorAll('.scrolling');
+    var navbar = document.querySelector('.jkkkkd');
+    var sectors = document.querySelectorAll('.sectorsdem')
+    navbar.classList.toggle('scroll-active', window.scrollY > 35);
+
+    sectors.forEach((sec, i) =>{
+        var top = window.scrollY;
+        var offset = sec.offsetTop;
+        var height = sec.offsetHeight;
+        var id = sec.getAttribute('id');
+
+        if(top > offset && top < offset + height){
+            scrolling.forEach((single, indico) =>{
+                single.classList.remove('active');
+                document.querySelector('.scrolling[href*=' +id +']').classList.add('active')
+            })
+        }
+    })
+})
+
+   class Firework{
+    constructor(x, y){
+        this.x = x + window.scrollX;
+        this.y = y + window.scrollY;
+        this.size = Math.floor(Math.random() * 20) + 5;
+        this.animationDuration = Math.random() * 2 + 20;
+        this.fadeOutDelay = Math.random() * 2 + 20;
+
+        this.file = document.createElement('div')
+        this.file.className = 'firework';
+        this.file.style.left = this.x + 'px';
+        this.file.style.top = this.y + 'px';
+        this.file.style.width = this.size + 'px';
+        this.file.style.height = this.size + 'px';
+        this.file.style.animationDuration = this.animationDuration + 's';
+
+        document.body.appendChild(this.file);
+
+        setTimeout(() =>{
+            this.fadeOut();
+        }, this.fadeOutDelay * 1000)
+    }
+    fadeOut() {
+        this.file.style.opacity = 0;
+        setTimeout(() =>{
+            this.file.remove();
+        }, 1000)       
+    }
+
+    static randomPosition(){
+        const x = Math.floor(Math.random() * window.innerWidth)
+        const y = Math.floor(Math.random() * window.innerHeight)
+        return {x, y}
+    }
+    static createRandomFirework(){
+        const {x, y} = Firework.randomPosition();
+        new Firework(x, y)
+    }
+}
+setInterval(() => {
+    Firework.createRandomFirework();
+}, 500);
+
+document.addEventListener('click', (event) =>{
+    const x = event.clientX;
+    const y = event.clientY;
+    new Firework(x, y);
+});
+let blacke = document.querySelector('.blacke')
+let men = new Date()
+let mens = men.getFullYear();
+    blacke.innerHTML = 'Utitbest Akpan - All right Reserved &copy; 2022 - ' + mens;  
+
+
+  
+
+// var progrm = document.querySelector('.progrm');
+// let boding = document.body;
+// document.body.onscroll = function(){
+//     var winscrool = this.scrollTop || this.scrollTop;
+//     var hga = this.scrollHeight - this.clientHeight;
+//     var csea = (winscrool / hga) * 100;
+//     document.querySelector('.progrm').style.width = csea + '%';
+//     // alert(csea)
+// }
